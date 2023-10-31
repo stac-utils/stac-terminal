5dd60bc2683556cec415273d2631969c3889aa93
jkeifer <jkeifer@element84.com>
2023-10-31 12:17:51 -0700

    fixup mistakes dynamically getting package version

--- src/stacterm/version.py
+++ src/stacterm/version.py
@@ -1,6 +1,6 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("dbami")
+    __version__ = version("stacterm")
 except PackageNotFoundError:
     __version__ = "0.0.0"
